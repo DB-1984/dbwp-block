ab4253075ffd37740537b8595492b7be11283b61
ePianos <epianos@ePianoss-MacBook-Pro.local>
2026-09-12 16:51:17 +0100

    Tweak to mobile overlay, fixed broken aos atts added by JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hero = document.querySelector("h1.sl-hero__headline");
+
   if (hero) {
-    hero.setAttribute("data-aos", "fade-down");
+    Object.assign(hero.dataset, {
+      aos: "fade-down",
+      aosDuration: "700",
+    });
   }
 
   const tag = document.querySelector(".sl-hero__subhead");
+
   if (tag) {
     Object.assign(tag.dataset, {
-      aos: "fade-in",
-      duration: "1000",
+      aos: "fade",
+      aosDuration: "1000",
     });
   }
 
   const services = document.querySelector(".services-shell");
+
   if (services) {
-    services.setAttribute("data-aos", "fade-left");
-  }
-
-  const servicesCta = document.querySelector(".services-cta");
-  if (servicesCta) {
-    servicesCta.setAttribute("data-aos", "fade-up");
-  }
-
-  const contactForm = document.querySelector(".contact-form-panel");
-  if (contactForm) {
-    Object.assign(contactForm.dataset, {
-      aos: "fade-in",
-      duration: "2000",
+    Object.assign(services.dataset, {
+      aos: "fade-left",
+      aosDuration: "900",
     });
   }
 
-  requestAnimationFrame(function () {
-    AOS.init({
+  const servicesCta = document.querySelector(".services-cta");
+
+  if (servicesCta) {
+    Object.assign(servicesCta.dataset, {
+      aos: "fade-up",
+      aosDuration: "700",
+    });
+  }
+
+  const contactForm = document.querySelector(".contact-form-panel");
+
+  if (contactForm) {
+    Object.assign(contactForm.dataset, {
+      aos: "fade",
+      aosDuration: "2000",
+    });
+  }
+
+  if (typeof window.AOS !== "undefined") {
+    window.AOS.init({
       duration: 700,
       once: true,
       offset: 60,
     });
-  });
+  } else {
+    console.error("AOS has not loaded.");
+  }
 });
